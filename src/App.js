74300b4c35f617e6b13f11dbe87940e7369a204b
PattyBarnas <patrykb.tcxr@gmail.com>
2023-01-28 00:58:41 -0600

    routing fix

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     <Router>
       <MainNavigation />
       <Routes>
-        <Route path="/" element={<Hero />} exact></Route>
+        <Route path="/firetek-react" element={<Hero />} exact></Route>
         <Route path="/services" element={<Services />}></Route>
       </Routes>
       <Footer />
